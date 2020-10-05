7c2c1ad5129f546fde578addc749a59e34181adb
deewaes <ismh5279@gmail.com>
2020-10-05 12:51:26 +0900

    10.05 숙제

--- src/main/java/kr/or/ddit/jsp/MulCalculation.java
+++ src/main/java/kr/or/ddit/jsp/MulCalculation.java
@@ -6,16 +6,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/mulCalculation")
 public class MulCalculation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("mulInput.jsp").forward(request, response);
+		request.getRequestDispatcher("/jsp/mulInput.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int param1 = Integer.parseInt(request.getParameter("param1"));
+		int param2 = Integer.parseInt(request.getParameter("param2"));
+		
+		int mulResult = param1*param2;
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("mulResult", mulResult);
+		
+		request.getRequestDispatcher("/jsp/mulResult.jsp").forward(request, response);
+		
 	}
 
 }
--- src/main/webapp/jsp/mulInput.jsp
+++ src/main/webapp/jsp/mulInput.jsp
@@ -7,9 +7,10 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<form method = "post" action="<%request.getContextPath(); %>">
+	<form method = "post" action="<%request.getContextPath(); %>/mulCalculation">
 		param1 : <input type = "text" name = "param1"><br>
 		param2 : <input type = "text" name = "param2"><br>
+		<input type="submit" value = "전송">
 	</form>
 </body>
 </html>
