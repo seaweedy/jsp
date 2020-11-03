004cf9e9155e1324050e1d1ac82a29c88fb13768
deewaes <ismh5279@gmail.com>
2020-11-03 15:18:23 +0900

    스프링 MVC  . dispatcher servlet 설정

--- src/main/java/kr/or/ddit/board/model/Person.java
+++ src/main/java/kr/or/ddit/board/model/Person.java
@@ -1,11 +1,30 @@
 package kr.or.ddit.board.model;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import kr.or.ddit.board.repository.BoardRepositoryI;
 
 public class Person {
+	public static void main(String[] args) {
+		Person person = new Person();
+	}
 	private int age;
 	private BoardRepositoryI boardRepository;
 	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date birthdate;
+	
+	@DateTimeFormat(pattern = "MM-dd-yyyy")
+	private Date hiredate;
+	
+	public Date getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
 	public int getAge() {
 		return age;
 	}
