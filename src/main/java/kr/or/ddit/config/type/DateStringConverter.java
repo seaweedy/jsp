c73a80641f4af903191ea0fc0f9533ea78e20bc0
deewaes <ismh5279@gmail.com>
2020-11-03 15:16:52 +0900

    스프링 MVC  . dispatcher servlet 설정

---
+++ src/main/java/kr/or/ddit/config/type/DateStringConverter.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.config.type;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class DateStringConverter implements Converter<String, Date>{
+
+	// 2020-11-03
+	@Override
+	public Date convert(String source) {
+		SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+		try {
+			date = transFormat.parse(source);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return date;
+	}
+
+}
