6637935a00c111c4bae9164171634f28f7e81b72
deewaes <ismh5279@gmail.com>
2020-11-08 22:53:42 +0900

    20201108 공부하며 오타 수정

--- src/test/java/kr/or/ddit/WebTestConfig.java
+++ src/test/java/kr/or/ddit/WebTestConfig.java
@@ -1,10 +1,6 @@
 package kr.or.ddit;
 
-import static org.junit.Assert.*;
-
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
