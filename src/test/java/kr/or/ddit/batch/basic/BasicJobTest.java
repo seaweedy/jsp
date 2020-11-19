4a7df4b597f605bbe9bd901b8f8783bc09bd521b
deewaes <ismh5279@gmail.com>
2020-11-19 17:29:17 +0900

    spring batch  . basicBatch  . repeatableBatch  . mybatis-spring Batch  . one query

---
+++ src/test/java/kr/or/ddit/batch/basic/BasicJobTest.java
@@ -0,0 +1,37 @@
+package kr.or.ddit.batch.basic;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations =
+	{"classpath:kr/or/ddit/config/spring/batch-context.xml"})
+public class BasicJobTest {
+
+	// batch 테스트 주의점
+	// spring 컨테이너에 job이 하나만 등록 되어 있어야함
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+	
+	@Test
+	public void basicJobTest() throws Exception {
+		/***Given***/
+		
+
+		/***When***/
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob(); // 반환타입 : 실행정보, 실행을 하는 메서드
+
+		/***Then***/
+		assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+		
+	}
+
+}
